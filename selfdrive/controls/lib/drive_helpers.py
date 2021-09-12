cb1f89d1a7e00797bd864b4c51618b32e39a02d4
Aragon7777 <78983073+Aragon7777@users.noreply.github.com>
2021-09-12 14:48:56 -0400

    5mph hold 1mph short press

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -43,20 +43,20 @@ def update_v_cruise(v_cruise_kph, buttonEvents, enabled, cur_time, accel_pressed
   
   if enabled:
     if accel_pressed:
-      if ((cur_time-accel_pressed_last) >= 0.5 or (fastMode and (cur_time-accel_pressed_last) >= 0.85)):
-        v_cruise_kph += 1
+      if ((cur_time-accel_pressed_last) >= 0.5 or (fastMode and (cur_time-accel_pressed_last) >= 0.25)):
+        v_cruise_kph += V_CRUISE_DELTA - (v_cruise_kph % V_CRUISE_DELTA)
     elif decel_pressed:
-      if ((cur_time-decel_pressed_last) >= 0.5 or (fastMode and (cur_time-decel_pressed_last) >= 0.85)):
-        v_cruise_kph -= 1
+      if ((cur_time-decel_pressed_last) >= 0.5 or (fastMode and (cur_time-decel_pressed_last) >= 0.25)):
+        v_cruise_kph -= V_CRUISE_DELTA - ((V_CRUISE_DELTA - v_cruise_kph) % V_CRUISE_DELTA)
     else:
       for b in buttonEvents:
         if not b.pressed:
           if b.type == car.CarState.ButtonEvent.Type.accelCruise:
             if (not fastMode):
-              v_cruise_kph += V_CRUISE_DELTA - (v_cruise_kph % V_CRUISE_DELTA)
+              v_cruise_kph += 1
           elif b.type == car.CarState.ButtonEvent.Type.decelCruise:
             if (not fastMode):
-              v_cruise_kph -= V_CRUISE_DELTA - ((V_CRUISE_DELTA - v_cruise_kph) % V_CRUISE_DELTA)
+              v_cruise_kph -= 1
     v_cruise_kph = clip(v_cruise_kph, V_CRUISE_MIN, V_CRUISE_MAX) 
 
   return v_cruise_kph
