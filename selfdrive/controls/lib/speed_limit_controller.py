0f7c34dfd928d736416c50254d89b5ee3f4f5094
Aragon7777 <78983073+Aragon7777@users.noreply.github.com>
2021-09-11 23:04:46 -0400

    Custom Speed Limit Offset

--- selfdrive/controls/lib/speed_limit_controller.py
+++ selfdrive/controls/lib/speed_limit_controller.py
@@ -15,8 +15,8 @@ _PARAMS_UPDATE_PERIOD = 2.  # secs. Time between parameter updates.
 _TEMP_INACTIVE_GUARD_PERIOD = 1.  # secs. Time to wait after activation before considering temp deactivation signal.
 
 # Lookup table for speed limit percent offset depending on speed.
-_LIMIT_PERC_OFFSET_V = [0.0, 0.1, 0.125,  0.2, 0.21, 0.23]  # 25, 33, 45, 60, 67, 70 mph
-_LIMIT_PERC_OFFSET_BP = [11.0, 13.4, 20.1, 22.3, 24.58, 29.0]  # 25, 30, 40 50, 55, 65 mph
+_LIMIT_PERC_OFFSET_V = [0.120, 0.167, 0.125,  0.04471, 0.06503, 0.1077, 0.1143]     # 28, 35, 45, 55, 63, 72, 78 mph // offset applied
+_LIMIT_PERC_OFFSET_BP = [11.176, 13.4112, 20.1, 22.352, 24.5872, 29.0576, 31.2928]  # 25, 30, 40, 50, 55, 65, 70 mph // actual speed limit
 
 #_LIMIT_PERC_OFFSET_V = [0.28, 0.038]  # 55, 105, 135 km/h 96, 129
 #_LIMIT_PERC_OFFSET_BP = [13.9, 36.1]  # 50, 100, 130 km/h
