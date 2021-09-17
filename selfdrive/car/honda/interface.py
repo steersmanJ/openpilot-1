52da610a91f07fdcc5e4d77f38d3f0df067d66ef
Aragon7777 <78983073+Aragon7777@users.noreply.github.com>
2021-09-17 09:25:15 -0400

    Update interface.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -158,7 +158,7 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.lqr.c = [1., 0.]
         ret.lateralTuning.lqr.k = [-110.73572306, 451.22718255]
         ret.lateralTuning.lqr.l = [0.3233671, 0.3185757]
-        ret.lateralTuning.lqr.dcGain = 0.004237852961363602 # Personal note: 0.00223 corners too early, 0.012 not enough.
+        ret.lateralTuning.lqr.dcGain = 0.002837852961363602 # Personal note: 0.00223 corners too early, 0.012 not enough.
       else:
         ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 2560], [0, 2560]]
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[1.1], [0.33]]
