f1ca785cbc66b0f70324b2745a402750bb3430f4
Aragon7777 <78983073+Aragon7777@users.noreply.github.com>
2021-10-14 14:40:19 -0400

    Hotfix

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -25,7 +25,6 @@ class CarInterface(CarInterfaceBase):
     self.last_enable_pressed = 0
     self.last_enable_sent = 0
 
-class CarInterface(CarInterfaceBase):
   @staticmethod
   def get_pid_accel_limits(CP, current_speed, cruise_speed):
     # NIDECs don't allow acceleration near cruise_speed,
