081ce3557df567c459a677dc01ccee4d2a596132
Casey Francis <>
2022-02-23 22:12:31 -0800

    Blue path
    Co-Authored-By: Comma Device <device@comma.ai>
    Co-Authored-By: Willem Melching <willem.melching@gmail.com>

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -93,7 +93,7 @@ protected:
   void updateFrameMat(int w, int h) override;
   void drawLaneLines(QPainter &painter, const UIScene &scene);
   void drawLead(QPainter &painter, const cereal::ModelDataV2::LeadDataV3::Reader &lead_data, const QPointF &vd);
-  inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
+  inline QColor redColor(int alpha = 255) { return QColor(39, 99, 255, alpha); }
   double prev_draw_t = 0;
 };
 
