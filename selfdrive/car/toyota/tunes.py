06a71c0d9671eebf74b99b20628994c84bf3a282
Aragon <toms0760@gmail.com>
2022-02-28 11:00:03 -0500

    Aragon's Modifications

--- selfdrive/car/toyota/tunes.py
+++ selfdrive/car/toyota/tunes.py
@@ -30,12 +30,10 @@ class LatTunes(Enum):
 def set_long_tune(tune, name):
   # Improved longitudinal tune
   if name == LongTunes.TSS2 or name == LongTunes.PEDAL:
-    tune.deadzoneBP = [0., 8.05]
-    tune.deadzoneV = [.0, .14]
-    tune.kpBP = [0., 5., 20.]
-    tune.kpV = [1.3, 1.0, 0.7]
-    tune.kiBP = [0., 5., 12., 20., 27.]
-    tune.kiV = [.35, .23, .20, .17, .1]
+    tune.kpBP = [0., 5., 35.]
+    tune.kpV = [.3, 0.2, 0.125]
+    tune.kiBP = [0., 35.]
+    tune.kiV = [0.09, 0.06]
   # Default longitudinal tune
   elif name == LongTunes.TSS:
     tune.deadzoneBP = [0., 9.]
